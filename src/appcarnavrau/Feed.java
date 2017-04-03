174635d4bc60699d220d5bb4dc46863bababc3fa
FSRenan <renan.022@hotmail.com>
2017-04-03 12:30:20 -0300

    Comit

--- src/appcarnavrau/Feed.java
+++ src/appcarnavrau/Feed.java
@@ -45,8 +45,7 @@ public class Feed extends javax.swing.JFrame {
     //Execução paralela de atualizar imagens do twitter
     private Runnable t1 = new Runnable() {
         public void run() {
-            ArrayList<URL> urls = new ArrayList(100000);
-
+            ArrayList<URL> urls = new ArrayList();
             //Faz autentificação para conexão com o twitter
             ConfigurationBuilder cf = new ConfigurationBuilder();
             //Faz a autentificação com os tokens de acesso
@@ -58,36 +57,36 @@ public class Feed extends javax.swing.JFrame {
 
             TwitterFactory tf = new TwitterFactory(cf.build());
             twitter4j.Twitter twitter = tf.getInstance();
-            //Filtras imagens com a hashtag
+            //Declaraçao de objeto tratamento tweets
             TratamentoTweets trata = new TratamentoTweets();
+            //Adiciona ao array urls das imagens relacionadas as tags
+            trata.pesquisarTag(twitter, "#Carnavrau", urls, 1000);
+            System.out.println(urls.size());
+            trata.pesquisarTag(twitter, "#Carnaval", urls, 1000);
+            System.out.println(urls.size());
+            trata.pesquisarTag(twitter, "#CarnavalSP", urls, 1000);
+            System.out.println(urls.size());
 
-            trata.pesquisarTag(twitter, "#ThankYouJustinBieber", urls, 1000);
-            System.out.println(urls.size());
-            trata.pesquisarTag(twitter, "#jb", urls, 1000);
-            System.out.println(urls.size());
-            trata.pesquisarTag(twitter, "#justinbieber", urls, 1000);
-            System.out.println(urls.size());
-            trata.pesquisarTag(twitter, "#justin", urls, 1000);
-            trata.pesquisarTag(twitter, "#show", urls, 1000);
-            trata.pesquisarTag(twitter, "#brasil", urls, 1000);
             //Exibe as urls no Array
             System.out.println("\n-----URLS NO ARRAY!");
             for (URL url : urls) {
                 System.out.println(url);
             }
             System.out.println("-----ArrayAcabou: " + urls.size());
-            Collections.shuffle(urls);
+           
             //________________________
 
             boolean atualizar = true;
+            //Atualizar as fotos nos labels
             while (atualizar) {
+                //Randomiza as urls
+                Collections.shuffle(urls);
                 int i = 0;
                 try {
-
                     while (urls.get(i) != null) {
 
                         lblFOTO4.setIcon(ajustarImagem(urls.get(i), lblFOTO4));
-//                 jPainel.setComponentZOrder(lblFOTO4, 2);
+
                         i++;
                         lblFOTO3.setIcon(ajustarImagem(urls.get(i), lblFOTO4));
 
@@ -101,7 +100,7 @@ public class Feed extends javax.swing.JFrame {
 
                         //Aguarda 5 segundos para atualizar
                         try {
-                            for (int k = 3; k >= 0; k--) {
+                            for (int k = 10; k >= 0; k--) {
                                 Thread.sleep(1000);
                                 lblCont.setText(k + "s");
                             }
@@ -141,9 +140,6 @@ public class Feed extends javax.swing.JFrame {
         lblFOTO3 = new javax.swing.JLabel();
         lblFOTO4 = new javax.swing.JLabel();
         lblFOTO1 = new javax.swing.JLabel();
-        lblIconeTempo = new javax.swing.JLabel();
-        lblTemperatura = new javax.swing.JLabel();
-        lblMensagem = new javax.swing.JLabel();
         btnGame = new javax.swing.JButton();
         btnPesquisaBlocos = new javax.swing.JButton();
         txtBloco1 = new javax.swing.JTextField();
@@ -160,33 +156,6 @@ public class Feed extends javax.swing.JFrame {
         setResizable(false);
 
         jPainel.setBackground(new java.awt.Color(255, 255, 255));
-
-        lblFOTO2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/appcarnavrau/sol.png"))); // NOI18N
-        lblFOTO2.setText("jLabel1");
-
-        lblFOTO3.setText("jLabel3");
-
-        lblFOTO4.setText("jLabel2");
-
-        lblFOTO1.setText("DFDFD");
-
-        lblIconeTempo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/appcarnavrau/sol.png"))); // NOI18N
-        lblIconeTempo.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseEntered(java.awt.event.MouseEvent evt) {
-                lblIconeTempoMouseEntered(evt);
-            }
-            public void mouseExited(java.awt.event.MouseEvent evt) {
-                lblIconeTempoMouseExited(evt);
-            }
-        });
-
-        lblTemperatura.setText("30º");
-
-        lblMensagem.addPropertyChangeListener(new java.beans.PropertyChangeListener() {
-            public void propertyChange(java.beans.PropertyChangeEvent evt) {
-                lblMensagemPropertyChange(evt);
-            }
-        });
 
         btnGame.setText("Game");
         btnGame.addActionListener(new java.awt.event.ActionListener() {
@@ -226,112 +195,93 @@ public class Feed extends javax.swing.JFrame {
         jPainelLayout.setHorizontalGroup(
             jPainelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPainelLayout.createSequentialGroup()
-                .addGap(380, 380, 380)
-                .addComponent(lblIconeTempo, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE))
-            .addGroup(jPainelLayout.createSequentialGroup()
                 .addGap(10, 10, 10)
                 .addComponent(txtBloco1, javax.swing.GroupLayout.PREFERRED_SIZE, 410, javax.swing.GroupLayout.PREFERRED_SIZE))
             .addGroup(jPainelLayout.createSequentialGroup()
-                .addGap(220, 220, 220)
-                .addComponent(lblFOTO4, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE))
-            .addGroup(jPainelLayout.createSequentialGroup()
-                .addGap(200, 200, 200)
-                .addComponent(lblMensagem, javax.swing.GroupLayout.PREFERRED_SIZE, 230, javax.swing.GroupLayout.PREFERRED_SIZE))
-            .addGroup(jPainelLayout.createSequentialGroup()
-                .addGap(220, 220, 220)
-                .addComponent(lblFOTO2, javax.swing.GroupLayout.PREFERRED_SIZE, 210, javax.swing.GroupLayout.PREFERRED_SIZE))
-            .addGroup(jPainelLayout.createSequentialGroup()
-                .addGap(20, 20, 20)
-                .addComponent(btnGame, javax.swing.GroupLayout.PREFERRED_SIZE, 120, javax.swing.GroupLayout.PREFERRED_SIZE))
-            .addGroup(jPainelLayout.createSequentialGroup()
-                .addGap(20, 20, 20)
-                .addComponent(lblInformacao, javax.swing.GroupLayout.PREFERRED_SIZE, 290, javax.swing.GroupLayout.PREFERRED_SIZE))
-            .addGroup(jPainelLayout.createSequentialGroup()
-                .addGap(150, 150, 150)
-                .addComponent(btnPesquisaBlocos))
-            .addGroup(jPainelLayout.createSequentialGroup()
                 .addGap(10, 10, 10)
-                .addComponent(txtBloco3, javax.swing.GroupLayout.PREFERRED_SIZE, 410, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addComponent(lblFOTO1, javax.swing.GroupLayout.PREFERRED_SIZE, 210, javax.swing.GroupLayout.PREFERRED_SIZE))
             .addGroup(jPainelLayout.createSequentialGroup()
                 .addGap(10, 10, 10)
                 .addComponent(lblFOTO3, javax.swing.GroupLayout.PREFERRED_SIZE, 210, javax.swing.GroupLayout.PREFERRED_SIZE))
             .addGroup(jPainelLayout.createSequentialGroup()
-                .addGap(290, 290, 290)
+                .addGap(390, 390, 390)
                 .addComponent(lblCont))
+            .addComponent(lblFundo, javax.swing.GroupLayout.PREFERRED_SIZE, 430, javax.swing.GroupLayout.PREFERRED_SIZE)
             .addGroup(jPainelLayout.createSequentialGroup()
-                .addGap(280, 280, 280)
-                .addComponent(btnSobre))
-            .addGroup(jPainelLayout.createSequentialGroup()
-                .addGap(410, 410, 410)
-                .addComponent(lblTemperatura))
+                .addGap(20, 20, 20)
+                .addComponent(btnGame, javax.swing.GroupLayout.PREFERRED_SIZE, 120, javax.swing.GroupLayout.PREFERRED_SIZE))
             .addGroup(jPainelLayout.createSequentialGroup()
                 .addGap(10, 10, 10)
                 .addComponent(txtBloco2, javax.swing.GroupLayout.PREFERRED_SIZE, 410, javax.swing.GroupLayout.PREFERRED_SIZE))
             .addGroup(jPainelLayout.createSequentialGroup()
                 .addGap(10, 10, 10)
-                .addComponent(lblFOTO1, javax.swing.GroupLayout.PREFERRED_SIZE, 210, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addComponent(txtBloco3, javax.swing.GroupLayout.PREFERRED_SIZE, 410, javax.swing.GroupLayout.PREFERRED_SIZE))
+            .addGroup(jPainelLayout.createSequentialGroup()
+                .addGap(220, 220, 220)
+                .addComponent(lblFOTO4, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE))
+            .addGroup(jPainelLayout.createSequentialGroup()
+                .addGap(150, 150, 150)
+                .addComponent(btnPesquisaBlocos))
+            .addGroup(jPainelLayout.createSequentialGroup()
+                .addGap(220, 220, 220)
+                .addComponent(lblFOTO2, javax.swing.GroupLayout.PREFERRED_SIZE, 210, javax.swing.GroupLayout.PREFERRED_SIZE))
+            .addGroup(jPainelLayout.createSequentialGroup()
+                .addGap(290, 290, 290)
+                .addComponent(btnSobre, javax.swing.GroupLayout.PREFERRED_SIZE, 70, javax.swing.GroupLayout.PREFERRED_SIZE))
+            .addGroup(jPainelLayout.createSequentialGroup()
+                .addGap(20, 20, 20)
+                .addComponent(lblInformacao, javax.swing.GroupLayout.PREFERRED_SIZE, 290, javax.swing.GroupLayout.PREFERRED_SIZE))
             .addComponent(lblQUADROFOTOS)
-            .addComponent(lblFundo, javax.swing.GroupLayout.PREFERRED_SIZE, 430, javax.swing.GroupLayout.PREFERRED_SIZE)
         );
         jPainelLayout.setVerticalGroup(
             jPainelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPainelLayout.createSequentialGroup()
-                .addComponent(lblIconeTempo)
-                .addGap(70, 70, 70)
+                .addGap(120, 120, 120)
                 .addComponent(txtBloco1, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(304, 304, 304)
+                .addGap(100, 100, 100)
+                .addComponent(lblFOTO1, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, 0)
+                .addComponent(lblFOTO3, javax.swing.GroupLayout.PREFERRED_SIZE, 210, javax.swing.GroupLayout.PREFERRED_SIZE))
+            .addComponent(lblCont, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addComponent(lblFundo, javax.swing.GroupLayout.PREFERRED_SIZE, 670, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addGroup(jPainelLayout.createSequentialGroup()
+                .addGap(10, 10, 10)
+                .addComponent(btnGame, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE))
+            .addGroup(jPainelLayout.createSequentialGroup()
+                .addGap(160, 160, 160)
+                .addComponent(txtBloco2, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE))
+            .addGroup(jPainelLayout.createSequentialGroup()
+                .addGap(200, 200, 200)
+                .addComponent(txtBloco3, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE))
+            .addGroup(jPainelLayout.createSequentialGroup()
+                .addGap(454, 454, 454)
                 .addComponent(lblFOTO4, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE))
             .addGroup(jPainelLayout.createSequentialGroup()
-                .addGap(80, 80, 80)
-                .addComponent(lblMensagem, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(10, 10, 10)
+                .addComponent(btnPesquisaBlocos, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE))
             .addGroup(jPainelLayout.createSequentialGroup()
                 .addGap(250, 250, 250)
                 .addComponent(lblFOTO2, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE))
             .addGroup(jPainelLayout.createSequentialGroup()
                 .addGap(10, 10, 10)
-                .addComponent(btnGame, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addComponent(btnSobre, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE))
             .addGroup(jPainelLayout.createSequentialGroup()
                 .addGap(90, 90, 90)
                 .addComponent(lblInformacao))
             .addGroup(jPainelLayout.createSequentialGroup()
-                .addGap(10, 10, 10)
-                .addComponent(btnPesquisaBlocos, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE))
-            .addGroup(jPainelLayout.createSequentialGroup()
-                .addGap(200, 200, 200)
-                .addComponent(txtBloco3, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE))
-            .addGroup(jPainelLayout.createSequentialGroup()
-                .addGap(450, 450, 450)
-                .addComponent(lblFOTO3, javax.swing.GroupLayout.PREFERRED_SIZE, 210, javax.swing.GroupLayout.PREFERRED_SIZE))
-            .addGroup(jPainelLayout.createSequentialGroup()
-                .addGap(50, 50, 50)
-                .addComponent(lblCont, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE))
-            .addGroup(jPainelLayout.createSequentialGroup()
-                .addGap(10, 10, 10)
-                .addComponent(btnSobre, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE))
-            .addGroup(jPainelLayout.createSequentialGroup()
-                .addGap(60, 60, 60)
-                .addComponent(lblTemperatura, javax.swing.GroupLayout.PREFERRED_SIZE, 18, javax.swing.GroupLayout.PREFERRED_SIZE))
-            .addGroup(jPainelLayout.createSequentialGroup()
-                .addGap(160, 160, 160)
-                .addComponent(txtBloco2, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE))
-            .addGroup(jPainelLayout.createSequentialGroup()
-                .addGap(250, 250, 250)
-                .addComponent(lblFOTO1, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE))
-            .addGroup(jPainelLayout.createSequentialGroup()
                 .addGap(240, 240, 240)
                 .addComponent(lblQUADROFOTOS, javax.swing.GroupLayout.PREFERRED_SIZE, 430, javax.swing.GroupLayout.PREFERRED_SIZE))
-            .addComponent(lblFundo, javax.swing.GroupLayout.PREFERRED_SIZE, 670, javax.swing.GroupLayout.PREFERRED_SIZE)
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPainel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jPainel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPainel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jPainel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
         );
 
         bindingGroup.bind();
@@ -339,18 +289,6 @@ public class Feed extends javax.swing.JFrame {
         pack();
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
-
-    private void lblMensagemPropertyChange(java.beans.PropertyChangeEvent evt) {//GEN-FIRST:event_lblMensagemPropertyChange
-
-    }//GEN-LAST:event_lblMensagemPropertyChange
-
-    private void lblIconeTempoMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_lblIconeTempoMouseEntered
-        lblMensagem.setText("Sol");
-    }//GEN-LAST:event_lblIconeTempoMouseEntered
-
-    private void lblIconeTempoMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_lblIconeTempoMouseExited
-        lblMensagem.setText("");
-    }//GEN-LAST:event_lblIconeTempoMouseExited
 
     private void txtBloco1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtBloco1ActionPerformed
         // TODO add your handling code here:
@@ -373,7 +311,6 @@ public class Feed extends javax.swing.JFrame {
 
             //Apaga a foto da pasta do projeto ao finalizar a execução da aplicação
             foto.deleteOnExit();
-
             System.out.println("Sucesso!");
         } catch (IOException e) {
             System.err.println("Erro imagem perfil!");
@@ -422,11 +359,8 @@ public class Feed extends javax.swing.JFrame {
     private javax.swing.JLabel lblFOTO3;
     private javax.swing.JLabel lblFOTO4;
     private javax.swing.JLabel lblFundo;
-    private javax.swing.JLabel lblIconeTempo;
     private javax.swing.JLabel lblInformacao;
-    private javax.swing.JLabel lblMensagem;
     private javax.swing.JLabel lblQUADROFOTOS;
-    private javax.swing.JLabel lblTemperatura;
     private javax.swing.JTextField txtBloco1;
     private javax.swing.JTextField txtBloco2;
     private javax.swing.JTextField txtBloco3;
--- src/appcarnavrau/TratamentoTweets.java
+++ src/appcarnavrau/TratamentoTweets.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package appcarnavrau;
 
 import java.net.URL;
@@ -12,34 +8,26 @@ import twitter4j.Query;
 import twitter4j.QueryResult;
 import twitter4j.Status;
 
-/**
- *
- * @author taciano
- */
+
 public class TratamentoTweets {
 
+    //Metodo que adc o no array as urls
     public void pesquisarTag(twitter4j.Twitter twitter, String tag, ArrayList<URL> urls, int num) {
         Query query = new Query("filter:images" + tag);
 
         try {
-
+            //numero de tweets solicitados
             int numberOfTweets = num;
             ArrayList<Status> tweets = new ArrayList<Status>();
-        
-            
             query.setCount(numberOfTweets);
-               
-               
             QueryResult result = twitter.search(query);
-
-         
-                tweets.addAll(result.getTweets());
+            //Filtras os tweets e pega apenas url de midias
+            tweets.addAll(result.getTweets());
                 for (Status status : tweets) {
                     MediaEntity[] media = status.getMediaEntities(); //get the media entities from the status
                     for (MediaEntity m : media) { //search trough your entities
-                     
                         URL url = new URL(m.getMediaURL());
-
+                        //Verifica no array se ja existe a url
                         if (!urls.contains(url)) {
                             urls.add(url);
                             System.out.println(url);
